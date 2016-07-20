889d4e734745d50edd63b6e64ab3fe6cc90e2c80
xiewang <xiewang0501@126.com>
2016-07-20 22:53:25 +0800

    loading (reverted from commit faa5bfa0ecb22c832ddec4704964585b9101ee96)

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
-import Main from './pages/navigator';
+import Main from './pages/home';
 
 const store = configureStore();
 
--- app/utils/common.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export function naviGoBack(navigator) {
-    if (navigator && navigator.getCurrentRoutes().length > 1) {
-        navigator.pop();
-        return true;
-    }
-    return false;
-}
