313c3745d2c30d20388a0b4cfc2e1e1da48c3d4c
lyan2 <lyan2@ebay.com>
2016-07-26 22:03:39 +0800

    add note for my home page.

--- app/pages/my/MyHomePage.js
+++ app/pages/my/MyHomePage.js
@@ -17,9 +17,20 @@ import {
 import Toolbar from '../../components/toolbar';
 import Button from '../../../app/components/button/Button';
 import Icon from '../../../node_modules/react-native-vector-icons/FontAwesome';
+import styles from './styles';
 
 var woman = <Icon style={{marginLeft:3,alignItems:'center'}} size={16} name="venus"/>;
 var man = <Icon style={{marginLeft:3,alignItems:'center'}} size={16} name="mars"/>;
+var rmbIcon = <Icon style={[styles.noteAssetIcon, {marginLeft:3}]} size={14} name="rmb"/>;
+var zanIcon = <Icon style={styles.noteAssetIcon} size={14} name="heart-o"/>;
+var commentIcon = <Icon style={styles.noteAssetIcon} size={14} name="comment-o"/>;
+var shoppingCartIcon = <Icon style={styles.noteAssetIcon} size={14} name="shopping-cart"/>;
+var ellipsisIcon = <Icon style={styles.noteAssetIcon} size={14} name="ellipsis-h"/>;
+
+var THUMB_URLS = [
+    require('../../assets/test/test.png'),
+    require('../../assets/test/test1.png')
+];
 
 export default class MyHomePage extends Component {
     constructor(props) {
@@ -53,7 +64,7 @@ export default class MyHomePage extends Component {
                     </View>
                     <View style={styles.income}>
                         <Text style={{fontSize:12}}>总收益:</Text>
-                        <Icon style={{marginLeft:3}} size={12} name="rmb"/>
+                        {rmbIcon}
                         <Text style={{marginLeft:3, fontSize:12, color:'#FC4D30'}}>32</Text>
                     </View>
                 </View>
@@ -83,97 +94,44 @@ export default class MyHomePage extends Component {
                 <View style={styles.myNotesTitle}>
                     <Text style={{fontSize:16}}>我的笔记</Text>
                 </View>
+
+                <View style={styles.myNote}>
+                    <View style={styles.noteUserBox}>
+                        <View style={[styles.portrait, {borderRadius:31}]}>
+                            <Image source={{uri: 'https://facebook.github.io/react/img/logo_small_2x.png', width: 31, height: 31}} />
+                        </View>
+                        <View style={styles.noteUserMsgBox}>
+                            <Text style={styles.noteUserTitle}>天才小熊猫</Text>
+                            <Text style={styles.noteCreateTime}>05-30 08:29</Text>
+                        </View>
+                    </View>
+                    <TouchableHighlight style={styles.noteThumbBox}>
+                        <Image style={styles.noteThumb} source={THUMB_URLS[0]} height={191} width={191}/>
+                    </TouchableHighlight>
+                    <Text style={styles.noteTitle}>最新入手的羊毛线</Text>
+                    <View style={styles.noteAssets}>
+                        <View style={styles.noteAsset}>
+                            {zanIcon}
+                            <Text style={[styles.text, {marginLeft:5}]}>11</Text>
+                        </View>
+                        <View style={styles.separator}></View>
+                        <View style={styles.noteAsset}>
+                            {commentIcon}
+                            <Text style={[styles.text, {marginLeft:5}]}>11</Text>
+                        </View>
+                        <View style={styles.separator}></View>
+                        <View style={styles.noteAsset}>
+                            {shoppingCartIcon}
+                            <Text style={[styles.text, {marginLeft:5}]}>({rmbIcon} 11)</Text>
+                        </View>
+                        <View style={styles.separator}></View>
+                        <View style={styles.noteAsset}>
+                            <Text style={[styles.text]}>...</Text>
+                        </View>
+                    </View>
+                </View>
             </View>
         );
     }
 }
 
-/*
- <Picker style={{width:80,height:26}}
- selectedValue={this.state.region}
- onValueChange={(lang) => this.setState({region: lang})}>
- <Picker.Item label="+86" value="China" />
- </Picker>*/
-
-var styles = StyleSheet.create({
-    text:{
-        color: '#4a4a4a'
-    },
-    userContainer: {
-        flexDirection:'column',
-        alignItems: 'center',
-    },
-    portrait: {
-        borderRadius: 45,
-        borderColor: '#fff',
-        borderWidth: 2
-    },
-    user: {
-        flexDirection:'row',
-        justifyContent: 'center',
-        marginTop:5
-    },
-    income: {
-        flexDirection:'row',
-        justifyContent: 'center',
-        alignItems:'flex-end',
-        marginTop:5
-    },
-    summaryContainer: {
-        flexDirection:'row',
-        justifyContent: 'center',
-        alignItems:'center',
-        backgroundColor:'#fff',
-        marginTop: 10,
-        marginBottom: 5,
-        paddingVertical:5
-    },
-    asset: {
-        flex:1,
-        flexDirection: 'column',
-        alignItems: 'center'
-    },
-    assetText: {
-        fontSize:12
-    },
-    separator: {
-        borderLeftWidth:1,
-        borderLeftColor:'#ccc',
-        height:48
-    },
-    count: {
-        fontSize:20,
-        color:'#FC4D30',
-        fontStyle: 'italic'
-    },
-    myNotesTitle: {
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-        backgroundColor: '#fff',
-        padding: 16
-    },
-    container: {
-        borderTopWidth: 1,
-        borderTopColor: '#ccc',
-        padding: 30,
-        marginTop: 21,
-        alignItems: 'stretch',
-    },
-    navigator: {
-        flexDirection: 'row',
-        justifyContent:'space-between'
-    },
-    button: {
-        paddingVertical:9,
-        backgroundColor: '#DFDFDF',
-        borderRadius:2,
-        fontSize:18,
-        color:'#fff',
-        fontFamily:'STHeitiSC-Medium',
-        alignItems:'center',
-        justifyContent:"center"
-    },
-    activeButton: {
-        backgroundColor: '#F37D30',
-    }
-});
---
+++ app/pages/my/styles.js
@@ -0,0 +1,138 @@
+/**
+ * Created by lyan2 on 16/7/26.
+ */
+import React from 'react-native'
+
+var {
+    StyleSheet
+} = React;
+
+export default styles = StyleSheet.create({
+    text:{
+        color: '#4a4a4a',
+        fontFamily: 'STHeitiSC-Light'
+    },
+    userContainer: {
+        flexDirection:'column',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    portrait: {
+        borderRadius: 45,
+        borderColor: '#fff',
+        borderWidth: 2
+    },
+    user: {
+        flexDirection:'row',
+        justifyContent: 'center',
+        marginTop:5
+    },
+    income: {
+        flexDirection:'row',
+        justifyContent: 'center',
+        alignItems:'flex-end',
+        marginTop:5
+    },
+    summaryContainer: {
+        flexDirection:'row',
+        justifyContent: 'center',
+        alignItems:'stretch',
+        backgroundColor:'#fff',
+        marginTop: 10,
+        marginBottom: 5,
+        paddingVertical:5
+    },
+    asset: {
+        flex:1,
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    assetText: {
+        fontSize:12
+    },
+    separator: {
+        borderLeftWidth:1,
+        borderLeftColor:'#ccc'
+    },
+    count: {
+        fontSize:20,
+        color:'#FC4D30',
+        fontStyle: 'italic'
+    },
+    myNotesTitle: {
+        backgroundColor: '#fff',
+        padding: 16
+    },
+    myNote:{
+        borderTopWidth: 1,
+        borderTopColor: '#ccc',
+        backgroundColor: '#fff',
+        padding: 20
+    },
+    noteUserBox: {
+        flexDirection: 'row'
+    },
+    noteUserMsgBox: {
+        marginLeft: 5,
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    },
+    noteUserTitle: {
+        fontSize: 16
+    },
+    noteCreateTime: {
+        fontSize: 12,
+        color: '#9B9B9B'
+    },
+    noteThumbBox: {
+        marginTop: 10,
+        flexDirection: 'row',
+        alignItems: 'stretch',
+        height: 191
+    },
+    noteThumb: {
+        
+    },
+    noteTitle: {
+        fontSize: 14,
+        marginVertical: 10
+    },
+    noteAssets: {
+        flexDirection: 'row',
+        alignItems: 'stretch',
+        justifyContent: 'center'
+    },
+    noteAsset: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    },
+    noteAssetIcon: {
+        color:'#FC4D30'  
+    },
+    container: {
+        borderTopWidth: 1,
+        borderTopColor: '#ccc',
+        padding: 30,
+        marginTop: 21,
+        alignItems: 'stretch',
+    },
+    navigator: {
+        flexDirection: 'row',
+        justifyContent:'space-between'
+    },
+    button: {
+        paddingVertical:9,
+        backgroundColor: '#DFDFDF',
+        borderRadius:2,
+        fontSize:18,
+        color:'#fff',
+        fontFamily:'STHeitiSC-Medium',
+        alignItems:'center',
+        justifyContent:"center"
+    },
+    activeButton: {
+        backgroundColor: '#F37D30',
+    }
+});
