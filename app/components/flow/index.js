adda670a842abf2705ede999f6c508f169a9d686
xiewang <xiewang0501@126.com>
2016-08-07 14:24:15 +0800

    UI for flow

--- app/components/flow/index.js
+++ app/components/flow/index.js
@@ -10,7 +10,8 @@ import {
     View,
     PixelRatio,
     Dimensions,
-    TouchableOpacity
+    TouchableOpacity,
+    TouchableWithoutFeedback
 } from 'react-native';
 
 import PrefetchImage from '../prefetchImage'
@@ -20,76 +21,74 @@ const thumbs = [
 
     {
         uri: 'http://hbimg.b0.upaiyun.com/fd7af3c379c8888a1ede4ea1046efea1fe953c63db770-sUI89w_fw658',
-            width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/0e343198bc21f4bfb6208dbb8e6d7d4358cffb3f2336c-M90TeP_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/be437a14550ce40dd0967e26bc4dd72dc2acdd88c418-TfAcUn_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/9944373939d4afd9b8c393973c3875f91ebf6c1d26d0e-lAUGG4_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/27e37520133ada3bde5c20d28bee40a5042f671711574-uM60Kq_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/3bfa27a07f27ab9a88493ca06cc21c001e3c943271027-VYWzdp_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/783b504aba16ddbc54e572d243ce70796c3bacd169828-uqStAZ_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/81329d6d0911921db04ee65f3df9d62aa6763b5f266fa-4kXDrj_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/cedb001478d5ad0dfe54a5af1797ad655efccd851d7d9-F0bnZJ_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/f065d8ce338b6f07a6b80471739c6739bb2c18979ada4-XKfULg_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/3720c87acb331491191a17d077f9b3f0e2705a69240ca-g1GUjG_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/7bb5cb03ac44b15d1dd5d2b9f20fc300c8d61252cf0f-BJ4TZf_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/21f167d573b123a69b6e7e52f0b68e1a374f13f165462-1CoVG0_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
     {
         uri: 'https://hbimg.b0.upaiyun.com/e5a9ab3f0ed21b2ad8758138b6f8c1f5f238c96ee506-FqVv3B_fw658',
-        width: (width/100)*47,
+        width: (width / 100) * 47,
         height: 200
     },
-
-
 
 
 ];
@@ -98,16 +97,17 @@ class Flow extends React.Component {
     constructor(props) {
         super(props);
         this._renderRow = this._renderRow.bind(this);
+        this._renderRow1 = this._renderRow1.bind(this);
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
             dataSource: ds.cloneWithRows(this._genRows({})),
             stars: ds.cloneWithRows([
-                '1','2','3','4','5'
+                '1', '2', '3', '4', '5'
             ])
         };
     }
 
-    _renderRow(rowData: string, sectionID: number, rowID: number) {
+    _renderRow(rowData:string, sectionID:number, rowID:number) {
         var rowHash = Math.abs(hashCode(rowData));
         var imgSource = thumbs[(rowHash) % thumbs.length];
         return (
@@ -120,19 +120,36 @@ class Flow extends React.Component {
                             resizeMode="cover"
                             width={(width/100)*47}
                             />
-                        <Text style={styles.text}>
-                            miya2016夏装新品宽松镂空短袖蕾丝衫女韩系显瘦性感度假上衣潮
-                        </Text>
-                        <View style={[{flexDirection:'row',width: (width/100)*47 -20,flex: 1,borderTopWidth: .6,paddingTop: 10,borderColor: '#9b9b9b',}]}>
-
-                            <Image style={{width:8 ,height:8}} source={require('../../assets/flow/star_filled.png')} />
-                            <Image style={{width:8 ,height:8}} source={require('../../assets/flow/star_filled.png')} />
-                            <Image style={{width:8 ,height:8}} source={require('../../assets/flow/star_filled.png')} />
-                            <Image style={{width:8 ,height:8}} source={require('../../assets/flow/star_filled.png')} />
-                            <Image style={{width:8 ,height:8}} source={require('../../assets/flow/star_filled.png')} />
-
-                            <Image style={{width:8 ,height:8, marginLeft:(width/100)*47 -100}} source={require('../../assets/flow/heart.png')}/>
-                            <Image style={{width:10 ,height:10, marginLeft:10}} source={require('../../assets/flow/comment.png')}/>
+                        <View style={styles.price}>
+                            <Text style={styles.priceText}>￥100</Text>
+                        </View>
+                        <TouchableWithoutFeedback>
+                            <View style={styles.portrait}>
+                                <Image
+                                    source={{uri: 'https://facebook.github.io/react/img/logo_small_2x.png', width: 30, height: 30}}/>
+                            </View>
+                        </TouchableWithoutFeedback>
+                        <View>
+                            <Text style={styles.text} lineBreakMode={'middle'}>
+                                miya2016夏装新品宽松镂空短袖蕾丝衫女韩系显瘦性感度假上衣潮
+                            </Text>
+                        </View>
+                        <View style={styles.interaction}>
+                            <View style={styles.star}>
+                                <Image source={require('../../assets/flow/star_filled.png')}/>
+                                <Image source={require('../../assets/flow/star_filled.png')}/>
+                                <Image source={require('../../assets/flow/star_filled.png')}/>
+                                <Image source={require('../../assets/flow/star_filled.png')}/>
+                                <Image source={require('../../assets/flow/star_unfilled.png')}/>
+                            </View>
+                            <View style={styles.like}>
+                                <Image source={require('../../assets/flow/heart.png')}/>
+                                <Text style={styles.interText}>11</Text>
+                            </View>
+                            <View style={styles.like}>
+                                <Image source={require('../../assets/flow/comment.png')}/>
+                                <Text style={styles.interText}>34</Text>
+                            </View>
                         </View>
                     </View>
                 </View>
@@ -140,9 +157,9 @@ class Flow extends React.Component {
         );
     }
 
-    _renderRow1(rowData: string, sectionID: number, rowID: number) {
+    _renderRow1(rowData:string, sectionID:number, rowID:number) {
         var rowHash = Math.abs(hashCode(rowData));
-        var imgSource = thumbs[(rowHash-2) % thumbs.length];
+        var imgSource = thumbs[(rowHash - 2) % thumbs.length];
         return (
             <TouchableOpacity onPress={() => this.props.press()} underlayColor="transparent" activeOpacity={0.5}>
                 <View>
@@ -153,19 +170,36 @@ class Flow extends React.Component {
                             resizeMode="cover"
                             width={(width/100)*47}
                             />
-                        <Text style={styles.text}>
-                            miya2016夏装新品宽松镂空短袖蕾丝衫女韩系显瘦性感度假上衣潮
-                        </Text>
-                        <View style={[{flexDirection:'row',width: (width/100)*47 -20,flex: 1,borderTopWidth: .6,paddingTop: 10,borderColor: '#9b9b9b',}]}>
-
-                            <Image style={{width:8 ,height:8}} source={require('../../assets/flow/star_filled.png')} />
-                            <Image style={{width:8 ,height:8}} source={require('../../assets/flow/star_filled.png')} />
-                            <Image style={{width:8 ,height:8}} source={require('../../assets/flow/star_filled.png')} />
-                            <Image style={{width:8 ,height:8}} source={require('../../assets/flow/star_filled.png')} />
-                            <Image style={{width:8 ,height:8}} source={require('../../assets/flow/star_filled.png')} />
-
-                            <Image style={{width:8 ,height:8, marginLeft:(width/100)*47 -100}} source={require('../../assets/flow/heart.png')}/>
-                            <Image style={{width:10 ,height:10, marginLeft:10}} source={require('../../assets/flow/comment.png')}/>
+                        <View style={styles.price}>
+                            <Text style={styles.priceText}>￥100</Text>
+                        </View>
+                        <TouchableWithoutFeedback>
+                            <View style={styles.portrait}>
+                                <Image
+                                    source={{uri: 'https://facebook.github.io/react/img/logo_small_2x.png', width: 30, height: 30}}/>
+                            </View>
+                        </TouchableWithoutFeedback>
+                        <View>
+                            <Text style={styles.text} lineBreakMode={'middle'}>
+                                miya2016夏装新品宽松镂空短袖蕾丝衫女韩系显瘦性感度假上衣潮
+                            </Text>
+                        </View>
+                        <View style={styles.interaction}>
+                            <View style={styles.star}>
+                                <Image source={require('../../assets/flow/star_filled.png')}/>
+                                <Image source={require('../../assets/flow/star_filled.png')}/>
+                                <Image source={require('../../assets/flow/star_filled.png')}/>
+                                <Image source={require('../../assets/flow/star_filled.png')}/>
+                                <Image source={require('../../assets/flow/star_unfilled.png')}/>
+                            </View>
+                            <View style={styles.like}>
+                                <Image source={require('../../assets/flow/heart.png')}/>
+                                <Text style={styles.interText}>11</Text>
+                            </View>
+                            <View style={styles.like}>
+                                <Image source={require('../../assets/flow/comment.png')}/>
+                                <Text style={styles.interText}>34</Text>
+                            </View>
                         </View>
                     </View>
                 </View>
@@ -173,7 +207,7 @@ class Flow extends React.Component {
         );
     }
 
-    _genRows(pressData: {[key: number]: boolean}): Array<string> {
+    _genRows(pressData:{[key: number]: boolean}):Array<string> {
         var dataBlob = [];
         for (var ii = 0; ii < 100; ii++) {
             var pressedText = pressData[ii] ? ' (X)' : '';
@@ -207,7 +241,7 @@ class Flow extends React.Component {
     }
 }
 
-var hashCode = function(str) {
+var hashCode = function (str) {
     var hash = 8;
     for (var ii = str.length - 1; ii >= 0; ii--) {
         hash = ((hash << 5) - hash) + str.charCodeAt(ii);
@@ -221,33 +255,85 @@ var styles = StyleSheet.create({
         flexDirection: 'column',
         flexWrap: 'wrap',
         alignSelf: 'flex-start',
-        width: (width/100)*50,
-        paddingLeft: width/100*2,
+        width: (width / 100) * 50,
+        paddingLeft: width / 100 * 2,
     },
     row: {
         flex: 1,
         marginTop: 8,
         marginBottom: 0,
-        width: (width/100)*47 ,
+        width: (width / 100) * 47,
         backgroundColor: '#fff',
         alignItems: 'center',
     },
     thumb: {
-        width: (width/100)*47 ,
+        width: (width / 100) * 47,
         height: 200,
         overflow: 'hidden',
     },
     text: {
         flex: 1,
-        margin: 10,
+        margin: 6,
         color: '#4a4a4a',
         fontSize: 11,
+        lineHeight: 13,
     },
     container: {
         flexDirection: 'row',
         flexWrap: 'wrap',
         alignSelf: 'flex-start',
         marginBottom: 60
+    },
+    portrait: {
+        backgroundColor: '#d8d8d8',
+        borderRadius: 30,
+        borderColor: '#fff',
+        borderWidth: 2,
+        marginTop: -15,
+        marginRight: 15,
+        alignSelf: 'flex-end',
+    },
+    price: {
+        position: 'absolute',
+        top: 20,
+        left: 0,
+        width: 70,
+        height: 25,
+        borderBottomRightRadius: 13,
+        borderTopRightRadius: 13,
+        backgroundColor: 'rgba(109, 109, 109, 0.8)',
+
+    },
+    priceText: {
+        color: '#fc7d30',
+        lineHeight: 20,
+        paddingLeft: 5,
+    },
+    interaction: {
+        flexDirection: 'row',
+        alignSelf: 'center',
+        width: (width / 100) * 47 - 20,
+        flex: 1,
+        borderTopWidth: .6,
+        paddingTop: 6,
+        marginBottom: 10,
+        borderColor: '#9b9b9b',
+    },
+    star: {
+        flexDirection: 'row',
+        flex: 1,
+    },
+    like: {
+        flexDirection: 'row',
+    },
+    comment: {
+        flexDirection: 'row',
+    },
+    interText: {
+        fontSize: 7,
+        paddingLeft: 4,
+        paddingRight: 4,
+        color: '#9b9b9b'
     }
 });
 
