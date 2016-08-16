ce3c580a45d88bc07799230429d93870065d9311
xiewang <xiewang0501@126.com>
2016-08-16 13:40:12 +0800

    detail page

--- app/pages/detail/index.js
+++ app/pages/detail/index.js
@@ -3,11 +3,21 @@
 import React  from 'react';
 import {
     View,
-    Text
+    Text,
+    Image,
+    ScrollView,
+    Dimensions,
+    TouchableOpacity
 } from 'react-native';
 import styles from './style';
 import Toolbar from '../../components/toolbar';
+import PrefetchImage from '../../components/prefetchImage';
 const shareImg = require('../../assets/note/transfer.png');
+const uri = ['https://hbimg.b0.upaiyun.com/fd0af542aae5ceb16f67c54c080a6537111d065b94beb-brWmWp_fw658',
+    'https://hbimg.b0.upaiyun.com/b13d086f8c1a3040ae05637c6cb283d60c1286661f43b-OKqo08_fw658',
+    'https://hbimg.b0.upaiyun.com/81329d6d0911921db04ee65f3df9d62aa6763b5f266fa-4kXDrj_fw658'
+];
+const {height, width} = Dimensions.get('window');
 
 class Detail extends React.Component {
     constructor(props) {
@@ -20,7 +30,7 @@ class Detail extends React.Component {
 
     render() {
         return(
-            <View style={{backgroundColor: '#fc7d30', flex: 1}}>
+            <View style={styles.container}>
                 <Toolbar
                     title="笔记详情"
                     navigator={this.props.navigator}
@@ -28,7 +38,41 @@ class Detail extends React.Component {
                     onLeftIconClicked={this._onLeftIconClicked(0)}
                     rightImg={shareImg}
                     />
-                <Text>detail</Text>
+                <ScrollView style={styles.main}>
+                    <View style={[styles.note,styles.block]}>
+                        <View style={styles.user}>
+                            <Image style={styles.portrait} source={{uri: 'https://facebook.github.io/react/img/logo_small_2x.png', width: 34, height: 34}}/>
+                            <View style={styles.info}>
+                                <Text style={styles.nick}>天才小熊猫</Text>
+                                <Text style={styles.date}>5月26日 11:29</Text>
+                            </View>
+                            <Image style={styles.follow} source={require('../../assets/note/follow.png')}/>
+                        </View>
+                        <View style={styles.thumbWarp}>
+                            <PrefetchImage
+                                imageUri={uri[1]}
+                                imageStyle={styles.thumb}
+                                resizeMode="cover"
+                                width={width}
+                                />
+                            <PrefetchImage
+                                imageUri={uri[1]}
+                                imageStyle={styles.thumb}
+                                resizeMode="cover"
+                                width={width}
+                                />
+                        </View>
+                        <View style={styles.description}>
+                            <Text style={[styles.dTitle,styles.baseText]}>miya2016夏装新品宽松镂空短袖</Text>
+                            <Text style={[styles.dContent,styles.baseText]}>天猫为第三方交易平台及互联网信息服务提供者，天猫（含网站、客户端等）所展示的商品/服务的标题、价格、详情等信息内容系由店铺经营者发布，其真实性、准确性和合法性均由店铺经营者负责。天猫提醒用户购买商品/服务前注意谨慎核实。如用户对商品/服务的标题、价格、详情等任何信息有任何疑问的，请在购买前通过阿里旺旺与店铺经营者沟通确认；天猫存在海量店铺，如用户发现店铺内有任何违法/侵权信息，请立即向天猫举报并提供有效线索。</Text>
+                        </View>
+                        <View style={styles.tags}>
+                            <TouchableOpacity style={styles.tag}><Text style={styles.tagText}>美人志</Text></TouchableOpacity>
+                            <TouchableOpacity style={styles.tag}><Text style={styles.tagText}>衣服</Text></TouchableOpacity>
+                            <TouchableOpacity style={styles.tag}><Text style={styles.tagText}>新款</Text></TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
         )
 
--- app/pages/detail/style.js
+++ app/pages/detail/style.js
@@ -1,13 +1,103 @@
 'use strict';
 
-import React from 'react-native';
+import React  from 'react';
+import {
+    StyleSheet,
+    Dimensions
+} from 'react-native';
 
-var {
-    StyleSheet
-    } = React;
+const {height, width} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-
+    baseText:{
+        fontSize: 14,
+        color: '#4a4a4a',
+        lineHeight: 18,
+        paddingBottom: 2
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#f1f1f1',
+    },
+    main: {
+        //padding: 10,
+        //marginBottom: 40
+    },
+    block: {
+        backgroundColor: '#fff',
+        marginBottom: 5,
+        flex: 1,
+        flexDirection: 'column',
+    },
+    user: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: 10,
+        paddingBottom: 0,
+    },
+    portrait: {
+        backgroundColor: '#d8d8d8',
+        borderRadius: 17,
+        borderColor: '#fff',
+        borderWidth: 1,
+    },
+    info: {
+        marginLeft: 5,
+    },
+    nick: {
+        fontSize: 16,
+        lineHeight: 16,
+        color: '#4a4a4a',
+    },
+    date: {
+        fontSize: 11,
+        lineHeight: 11,
+        marginTop: 5,
+        color: '#9b9b9b',
+    },
+    follow: {
+        position: 'absolute',
+        right: 10,
+        top: 14,
+    },
+    thumbWarp: {
+        marginTop: 10,
+        flexDirection: 'row',
+        overflow: 'visible',
+        flexWrap: 'nowrap',
+        alignSelf: 'flex-start',
+    },
+    thumb: {
+        width: width,
+        overflow: 'hidden',
+    },
+    description: {
+        padding: 10,
+    },
+    dTitle: {
+        paddingBottom: 8
+    },
+    tags: {
+        padding: 10,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignSelf: 'flex-start',
+    },
+    tag: {
+        backgroundColor: 'rgba(155,155,155,0.1)',
+        height: 25,
+        borderRadius: 2,
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginRight: 5,
+        marginBottom: 10,
+    },
+    tagText: {
+        fontSize: 12,
+        lineHeight: 18,
+        color: '#9b9b9b'
+    }
 });
 
 export default styles;
