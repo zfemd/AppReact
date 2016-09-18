f0232b68e27ca36193bdd331b02fee228bfae5b4
xiewang <xiewang0501@126.com>
2016-09-18 22:31:43 +0800

    search in invite page

--- app/pages/addFriends/index.js
+++ app/pages/addFriends/index.js
@@ -5,21 +5,46 @@ import {
     View,
     Text,
     TouchableOpacity,
-    TextInput
+    TextInput,
+    Image
 } from 'react-native';
 import styles from './style';
 import Toolbar from '../../components/toolbar';
 import Icon from 'react-native-vector-icons/Ionicons';
+import ImageButton from '../../components/toolbar/ImageButton.js';
+import { naviGoBack } from '../../utils/common';
+var backImg = require('../../assets/upload/rg_left.png');
 
 class Friends extends React.Component {
     constructor(props) {
         super(props);
+        this._onLeftIconClicked = this._onLeftIconClicked.bind(this);
+    }
+
+    _onLeftIconClicked() {
+        const { navigator } = this.props;
+        if (navigator) {
+            naviGoBack(navigator);
+        }
     }
 
     render() {
         return(
             <View style={styles.container}>
-
+                <View style={styles.search}>
+                    <ImageButton
+                        source={backImg}
+                        style={styles.back}
+                        onPress={this._onLeftIconClicked}
+                        />
+                    <TextInput
+                        style={styles.searchText}
+                        placeholder={'搜索通讯录好友'}
+                        placeholderTextColor='#bebebe'
+                        multiline={false}
+                        />
+                    <Image style={styles.magnifier} source={require('../../assets/invite/search.png')}/>
+                </View>
             </View>
         )
 
--- app/pages/addFriends/style.js
+++ app/pages/addFriends/style.js
@@ -3,24 +3,53 @@
 import React from 'react-native';
 
 var {
-    StyleSheet
+    StyleSheet,
+    Dimensions
     } = React;
 
+const {height, width} = Dimensions.get('window');
+
 const styles = StyleSheet.create({
-    baseText:{
+    baseText: {
         fontSize: 13,
         color: '#4a4a4a',
         lineHeight: 18,
         paddingBottom: 2
     },
-    dimText:{
+    dimText: {
         color: '#9b9b9b',
     },
     container: {
         flex: 1,
         flexDirection: 'column',
         backgroundColor: '#f1f1f1',
-    } 
+    },
+    search: {
+        height: 62,
+        paddingTop: 20,
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+    },
+    searchText: {
+        height: 24,
+        width: width - 66,
+        marginTop: 10,
+        marginLeft: 20,
+        paddingLeft: 26,
+        fontSize: 14,
+        lineHeight: 24,
+        borderWidth: 0,
+        backgroundColor: 'rgba(155,155,155,0.1)',
+    },
+    back: {
+        marginTop: 12,
+        marginLeft: 10
+    },
+    magnifier: {
+        position: 'absolute',
+        marginTop: 14,
+        marginLeft: 72 - width
+    }
 });
 
 export default styles;
