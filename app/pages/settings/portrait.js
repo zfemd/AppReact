a84aa5d66fada62856e827ec9af55b9a73b31146
xiewang <xiewang0501@126.com>
2016-11-24 22:37:46 +0800

    update portrait

--- app/pages/settings/portrait.js
+++ app/pages/settings/portrait.js
@@ -11,12 +11,23 @@ import {
 import styles from './style';
 import Toolbar from '../../components/toolbar';
 import { naviGoBack } from '../../utils/common';
+import UpdatePortraitPage from './updatePortrait';
 
 var {height, width} = Dimensions.get('window');
 
 class Portrait extends React.Component {
     constructor(props) {
         super(props);
+    }
+
+    _goToUpdatePage() {
+        const { navigator } = this.props;
+        if(navigator) {
+            navigator.push({
+                name: 'UpdatePortraitPage',
+                component: UpdatePortraitPage,
+            })
+        }
     }
 
     render() {
@@ -29,7 +40,7 @@ class Portrait extends React.Component {
                 <View style={styles.fullPortrait}>
                     <Image  style={[styles.fullPortraitImg]} source={{uri: info.thumbUri, width: pWidth, height: pHeight}} />
                 </View>
-                <TouchableOpacity style={styles.button} >
+                <TouchableOpacity style={styles.button} onPress={this._goToUpdatePage.bind(this)}>
                     <Text style={styles.buttonFont}>更换头像</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={[styles.button,styles.buttonGrey]} onPress={()=>naviGoBack(this.props.navigator)}>
---
+++ app/pages/settings/updatePortrait.js
@@ -0,0 +1,182 @@
+import React, { Component } from 'react';
+import {
+    CameraRoll,
+    Dimensions,
+    Image,
+    Navigator,
+    Platform,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View,
+    ImageStore,
+    ImageEditor
+} from 'react-native';
+import Toast from 'react-native-root-toast';
+import ImagePicker from 'react-native-image-picker';
+import { connect } from 'react-redux';
+import Toolbar from '../../components/toolbar';
+import PhoneLib from '../../components/camera/PhoneLib';
+import StoreActions from '../../constants/actions';
+import ImageButton from '../../components/toolbar/ImageButton';
+const arrowImg = require('../../assets/header/arrow.png');
+import styles from '../note/style';
+import { request, Token, toast } from '../../utils/common';
+
+
+class UpdatePortrait extends Component {
+    constructor(props, context) {
+        super(props);
+
+        this.state = {};
+
+        this.cameraOptions = {
+            title: '选择照片',
+            storageOptions: {
+                skipBackup: true,
+                path: 'images'
+            }
+        };
+
+        this.phoneLibOptions = {
+            title: '选择照片',
+            storageOptions: {
+                skipBackup: true,
+                path: 'images'
+            }
+        }
+    }
+
+    _onPressCamera() {
+        // The first arg is the options object for customization (it can also be null or omitted for default options),
+        // Launch Camera:
+        ImagePicker.launchCamera(this.cameraOptions, (response)  => {
+            if (response.didCancel) {
+            } else if (response.error) {
+                Toast.show(response.error, {duration: Toast.durations.SHORT, position: Toast.positions.CENTER});
+            } else if (response.customButton) {
+            } else {
+                // You can display the image using either data...
+                //let source = {uri: 'data:image/jpeg;base64,' + response.data, isStatic: true};
+
+                this.setState({
+                    avatarSource: response
+                });
+            }
+        });
+    }
+
+    _onPressImageLib() {
+        // Open Image Library:
+        ImagePicker.launchImageLibrary(this.phoneLibOptions, (response)  => {
+            if (response.didCancel) {
+            } else if (response.error) {
+                Toast.show(response.error, {duration: Toast.durations.SHORT, position: Toast.positions.CENTER});
+            } else if (response.customButton) {
+            } else {
+                // You can display the image using either data...
+                //let source = {uri: 'data:image/jpeg;base64,' + response.data, isStatic: true};
+                this.setState({
+                    avatarSource: response
+                });
+            }
+        });
+    }
+
+    _onPressImage(imageNode) {
+        this.setState({
+            avatarSource: imageNode.image
+        });
+    }
+
+    _getFirstImage(data) {
+        if (data != null) {
+            let assets = data.edges;
+            if (assets.length > 0) {
+                this.setState({avatarSource: assets[0].node.image});
+            }
+        }
+    }
+
+
+    _onContinue() {
+        const photo = this.state.avatarSource;
+        const imageSize = {width,height} = photo;
+
+        ImageEditor.cropImage(photo.uri, {offset: {x: 0, y: 0}, size: imageSize, displaySize: imageSize}, (url) => {
+            ImageStore.getBase64ForTag(url, (base64) => {
+                base64 = 'data:image/jpg;base64,' + base64;
+                let body = {
+                    image: base64
+                };
+                body = JSON.stringify(body);
+                Token.getToken(navigator).then((token) => {
+                    if (token) {
+                        request('/user/settings/personal-information/portrait', 'POST', body, token)
+                            .then((res) => {
+                                if (res.resultCode === 0) {
+                                    toast('修改头像成功')
+                                }
+                            }, function (error) {
+                                console.log(error);
+                            })
+                            .catch(() => {
+                                console.log('network error');
+                            });
+                    }
+                });
+            }, (error) => {
+                console.log(error);
+            });
+        }, (error) => {
+            console.log(error);
+        });
+
+
+    }
+
+    componentDidMount() {
+        // only fetch JPEG images.
+        var fetchParams:Object = {
+            first: 1,
+            groupTypes: 'SavedPhotos',
+            assetType: "Photos",
+            mimeTypes: ['image/jpeg']
+        };
+
+        if (Platform.OS === 'android') {
+            // not supported in android
+            delete fetchParams.groupTypes;
+        }
+
+        CameraRoll.getPhotos(fetchParams)
+            .then((data) => this._getFirstImage(data), (e) => logError(e));
+    }
+
+    render() {
+        let {height, width} = Dimensions.get('window');
+        height -= 21; // top navigator
+
+        return (
+            <View style={[styles.container, {height}, Platform.OS === 'android' ? null : {marginTop: 21}]}>
+                <Toolbar
+                    title="所有照片"
+                    onTitlePress={this._onPressImageLib.bind(this)}
+                    navigator={this.props.navigator}
+                    hideDrop={true}
+                    rightText='上传'
+                    onRightIconClicked={this._onContinue.bind(this)}
+                    />
+                <View style={{marginBottom: 4}}>
+                    <Image source={this.state.avatarSource} style={styles.uploadAvatar} width={width} height={200}/>
+                </View>
+                <PhoneLib contentContainerStyle={{flex:1}} navigator={this.props.navigator}
+                          onPressCamera={this._onPressCamera} onPressImage={this._onPressImage.bind(this)}/>
+
+            </View>
+        );
+    }
+}
+
+
+export default UpdatePortrait;
