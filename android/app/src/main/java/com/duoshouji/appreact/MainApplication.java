6bb104bdb796a6f15e6c6a8ae233981f08bdfa09
xiewang <xiewang0501@126.com>
2017-01-08 00:13:41 +0800

    add module react-native-wx

--- android/app/src/main/java/com/duoshouji/appreact/MainApplication.java
+++ android/app/src/main/java/com/duoshouji/appreact/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import cn.reactnative.modules.wx.WeChatPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new WeChatPackage(),
                     new WebViewBridgePackage(),
                     new HttpCachePackage(),
                     new RNSpinkitPackage(),
