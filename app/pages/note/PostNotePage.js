4273de064d6251c98e6ed517af4eb8bb20f33602
xiewang <xiewang0501@126.com>
2017-03-27 23:47:36 +0800

    title statistics

--- app/pages/note/PostNotePage.js
+++ app/pages/note/PostNotePage.js
@@ -42,7 +42,8 @@ class PostNotePage extends Component {
             address: '正在尝试获取当前位置...',
             draftNote: this.props.draftNote,
             token: null,
-            posting: false
+            posting: false,
+            titleLength: 30
         };
 
         Token.getToken(navigator).then((token) => {
@@ -55,30 +56,32 @@ class PostNotePage extends Component {
 
     componentDidMount() {
         let that = this;
-        Geolocation.getCurrentPosition(function(position){
+        Geolocation.getCurrentPosition(function (position) {
             let {coords} = position;
             let url = 'http://api.map.baidu.com/geocoder/v2/?output=json&ak=D8c7c1411571551ef8fe556f08c594bd&location=' + coords.latitude + ',' + coords.longitude;
 
-            fetch(url, { method: 'GET' })
-            .then((response) => response.json())
-            .then((responseJson) => {
-                let address = responseJson.result.formatted_address;
-                if (!address) {
-                    address = '定位失败';
-                }
-                that.setState({address: address, latitude:coords.latitude, longitude: coords.longitude});
-            })
-            .catch((error) => {
-                    Toast.show('可能是网络问题，无法定位。', {duration:Toast.durations.SHORT, position:Toast.positions.CENTER});
+            fetch(url, {method: 'GET'})
+                .then((response) => response.json())
+                .then((responseJson) => {
+                    let address = responseJson.result.formatted_address;
+                    if (!address) {
+                        address = '定位失败';
+                    }
+                    that.setState({address: address, latitude: coords.latitude, longitude: coords.longitude});
+                })
+                .catch((error) => {
+                    Toast.show('可能是网络问题，无法定位。', {duration: Toast.durations.SHORT, position: Toast.positions.CENTER});
                     that.setState({address: '定位失败'});
-            });
-        }, function(error){
+                });
+        }, function (error) {
             that.setState({address: '定位失败'});
         }, {
             timeout: 20000,
             maximumAge: 1000,
             enableHighAccuracy: true
         });
+
+        this._statCount(this.state.title);
     }
 
     _onCancel() {
@@ -86,7 +89,7 @@ class PostNotePage extends Component {
 
         if (navigator) {
             // remove draft note since user canceled.
-            dispatch({type:StoreActions.RESET_DRAFT_NOTE});
+            dispatch({type: StoreActions.RESET_DRAFT_NOTE});
             navigator.popToTop();
         }
     }
@@ -94,7 +97,7 @@ class PostNotePage extends Component {
     _addMorePhoto() {
         const { navigator } = this.props;
 
-        if(navigator) {
+        if (navigator) {
             navigator.push({
                 name: 'SelectPhotoPage',
                 component: SelectPhotoPage
@@ -111,7 +114,9 @@ class PostNotePage extends Component {
 
         if (data.images.length <= 0) return;
 
-        data.images = data.images.map(image => {return {"image":image.image, "marks":image.marks}});
+        data.images = data.images.map(image => {
+            return {"image": image.image, "marks": image.marks}
+        });
 
         fetch(configs.imageServiceUrl + 'notes/' + noteId + '/images', {
             method: 'POST',
@@ -126,14 +131,14 @@ class PostNotePage extends Component {
         }).then((responseJson) => {
             if (responseJson.resultCode == 0) {
                 // remove draft note since it has been post to server.
-                dispatch({type:StoreActions.RESET_DRAFT_NOTE});
+                dispatch({type: StoreActions.RESET_DRAFT_NOTE});
                 this.state.posting = false;
 
-                if(navigator) {
+                if (navigator) {
                     navigator.popToTop();
                 }
             } else {
-                Toast.show('抱歉，笔记图片发布失败。', {duration:Toast.durations.SHORT, position:Toast.positions.CENTER});
+                Toast.show('抱歉，笔记图片发布失败。', {duration: Toast.durations.SHORT, position: Toast.positions.CENTER});
                 this.state.posting = false;
             }
         }).catch((error) => {
@@ -141,7 +146,7 @@ class PostNotePage extends Component {
             this.state.posting = false;
         });
 
-        Toast.show('笔记正在发布，图片数量不同，发送时间可长可短，请耐心等待。', {duration:Toast.durations.LONG, position:Toast.positions.CENTER});
+        Toast.show('笔记正在发布，图片数量不同，发送时间可长可短，请耐心等待。', {duration: Toast.durations.LONG, position: Toast.positions.CENTER});
     }
 
     _sendNote() {
@@ -149,7 +154,7 @@ class PostNotePage extends Component {
         this.state.posting = true;
 
         if (this.state.draftNote == null || this.state.draftNote.notePhotos == null) {
-            Toast.show('发送笔记前，请先选择需要上传的图片。', {duration:Toast.durations.SHORT, position:Toast.positions.CENTER});
+            Toast.show('发送笔记前，请先选择需要上传的图片。', {duration: Toast.durations.SHORT, position: Toast.positions.CENTER});
             return;
         }
 
@@ -164,7 +169,7 @@ class PostNotePage extends Component {
         console.log(data);
 
         if (!data.title || !data.content) {
-            Toast.show('发布笔记需要您输入标题和内容。', {duration:Toast.durations.SHORT, position:Toast.positions.CENTER});
+            Toast.show('发布笔记需要您输入标题和内容。', {duration: Toast.durations.SHORT, position: Toast.positions.CENTER});
             return;
         }
 
@@ -184,11 +189,11 @@ class PostNotePage extends Component {
             if (responseJson && responseJson.resultCode == 0 && responseJson.resultValues) {
                 this._sendPhotos(responseJson.resultValues.noteId);
             } else {
-                Toast.show('发送笔记失败。', {duration:Toast.durations.SHORT, position:Toast.positions.CENTER});
+                Toast.show('发送笔记失败。', {duration: Toast.durations.SHORT, position: Toast.positions.CENTER});
                 this.state.posting = false;
             }
         }).catch((error) => {
-            Toast.show('发送失败，可能是网络中断。', {duration:Toast.durations.SHORT, position:Toast.positions.CENTER});
+            Toast.show('发送失败，可能是网络中断。', {duration: Toast.durations.SHORT, position: Toast.positions.CENTER});
             this.state.posting = false;
         });
     }
@@ -209,7 +214,7 @@ class PostNotePage extends Component {
     _onPressPhoto(index) {
         const { navigator } = this.props;
 
-        if(navigator) {
+        if (navigator) {
             navigator.push({
                 name: 'PhotosReviewPage',
                 component: PhotosReviewPage,
@@ -219,12 +224,17 @@ class PostNotePage extends Component {
         }
     }
 
+    _statCount(title) {
+        this.setState({titleLength: 30 - title.length});
+        this.state.title = title;
+    }
+
     _renderSelectedPhotos() {
         let {height, width} = Dimensions.get('window');
         let that = this;
         let morePhoto = (
             <TouchableHighlight key='morePhoto' style={styles.morePhotoBox} onPress={this._addMorePhoto.bind(this)}>
-                <Icon size={16} name="plus" color={colors.gray} />
+                <Icon size={16} name="plus" color={colors.gray}/>
             </TouchableHighlight>
         );
 
@@ -233,13 +243,15 @@ class PostNotePage extends Component {
         let photoRows = [];
         let photosPerRow = 4;
         let rowIndex = 0;
-        let {imageWidth, imageHeight} = {imageWidth: (width - 60) / photosPerRow, imageHeight:80};
+        let {imageWidth, imageHeight} = {imageWidth: (width - 60) / photosPerRow, imageHeight: 80};
 
         if (notePhotos != null && notePhotos.length > 0) {
-            notePhotos.forEach(function(notePhoto, index){
+            notePhotos.forEach(function (notePhoto, index) {
                 let image = (
-                    <TouchableHighlight key={notePhoto.photo.image.uri+index} onPress={() => that._onPressPhoto.call(that, index)} >
-                        <Image source={{uri:notePhoto.image}} style={styles.uploadAvatar} width={imageWidth} height={imageHeight} />
+                    <TouchableHighlight key={notePhoto.photo.image.uri+index}
+                                        onPress={() => that._onPressPhoto.call(that, index)}>
+                        <Image source={{uri:notePhoto.image}} style={styles.uploadAvatar} width={imageWidth}
+                               height={imageHeight}/>
                     </TouchableHighlight>
                 );
                 photos.push(image);
@@ -249,8 +261,9 @@ class PostNotePage extends Component {
         photos.push(morePhoto);
 
         rowIndex = Math.ceil(photos.length / photosPerRow) - 1;
-        for(let i = 0; i <= rowIndex; i++) {
-            let row = <View key={i} style={{flexDirection:'row', paddingVertical: 5}}>{this._renderPhotosRow(photos, photosPerRow, i * photosPerRow)}</View>;
+        for (let i = 0; i <= rowIndex; i++) {
+            let row = <View key={i}
+                            style={{flexDirection:'row', paddingVertical: 5}}>{this._renderPhotosRow(photos, photosPerRow, i * photosPerRow)}</View>;
             photoRows.push(row);
         }
 
@@ -271,31 +284,34 @@ class PostNotePage extends Component {
                     onRightIconClicked={this._onCancel.bind(this)}
                     />
 
-                <View style={{borderBottomWidth: 1, borderBottomColor: '#ccc', flexDirection: 'row', paddingVertical:10, margin: 15}}>
+                <View
+                    style={{borderBottomWidth: 1, borderBottomColor: '#ccc', flexDirection: 'row', paddingVertical:10, margin: 15}}>
                     <TextInput ref='titleInput' placeholder='添加标题' defaultValue={this.state.title}
                                clearButtonMode='while-editing' underlineColorAndroid='transparent'
-                               onChangeText={(value) => this.state.title = value}
+                               onChangeText={(value) => this._statCount(value)}
                                returnKeyType="next" maxLength={30} style={[styles.textInput, {flex:1}]}/>
-                    <Text>30</Text>
+                    <Text>{this.state.titleLength}</Text>
                 </View>
                 <View style={{flexDirection: 'row', paddingVertical:10, marginHorizontal: 15,height: 100}}>
                     <TextInput ref='contentInput' placeholder='说点你的心得吧' defaultValue={this.state.content}
                                clearButtonMode='while-editing' underlineColorAndroid='transparent'
-                               returnKeyType="next" multiline={true} numberOfLines = {8} style={[styles.textInput, {flex:1}]}
-                               onChangeText={(value) => this.state.content = value} />
+                               returnKeyType="next" multiline={true} numberOfLines={8}
+                               style={[styles.textInput, {flex:1}]}
+                               onChangeText={(value) => this.state.content = value}/>
                 </View>
-                <View style={[{borderBottomWidth: 1, borderBottomColor: '#ccc', paddingVertical:10, marginHorizontal: 15}]}>
+                <View
+                    style={[{borderBottomWidth: 1, borderBottomColor: '#ccc', paddingVertical:10, marginHorizontal: 15}]}>
                     {this._renderSelectedPhotos()}
                 </View>
 
                 <View style={{flexDirection: 'row', alignItems: 'center', padding:10, marginHorizontal: 15}}>
-                    <Image source={locationImg} style={{marginRight: 10}} />
+                    <Image source={locationImg} style={{marginRight: 10}}/>
                     <Text>发布于：</Text>
                     <Text style={{color: colors.orange}}>{this.state.address}</Text>
                 </View>
 
                 <TouchableHighlight onPress={this._sendNote.bind(this)}
-                    style={{padding: 15, justifyContent:'center', backgroundColor: colors.orange, flexDirection: 'row', position: 'absolute', bottom: 0, left: 0, right: 0}}>
+                                    style={{padding: 15, justifyContent:'center', backgroundColor: colors.orange, flexDirection: 'row', position: 'absolute', bottom: 0, left: 0, right: 0}}>
                     <Text style={{color: '#fff', fontSize:18}}>发布</Text>
                 </TouchableHighlight>
             </View>
